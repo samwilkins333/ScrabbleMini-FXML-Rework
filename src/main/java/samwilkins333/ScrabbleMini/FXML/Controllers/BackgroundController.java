95f7e11a30674861c71d6c38ff915f247e83b143
Sam Wilkins <samwilkins333@gmail.com>
2019-05-09 18:21:22 -0400

    observable image create refactor

--- src/main/java/samwilkins333/ScrabbleMini/FXML/Controllers/BackgroundController.java
+++ src/main/java/samwilkins333/ScrabbleMini/FXML/Controllers/BackgroundController.java
@@ -3,8 +3,8 @@ package main.java.samwilkins333.ScrabbleMini.FXML.Controllers;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
-import main.java.samwilkins333.ScrabbleMini.FXML.Utilities.ImageUtils;
-import main.java.samwilkins333.ScrabbleMini.FXML.Utilities.ObservableImage;
+import main.java.samwilkins333.ScrabbleMini.FXML.Scenes.Bindings.BindingMode;
+import main.java.samwilkins333.ScrabbleMini.FXML.Utilities.Image.ObservableImage;
 import main.java.samwilkins333.ScrabbleMini.Main;
 
 import java.net.URL;
@@ -19,7 +19,8 @@ public class BackgroundController implements Initializable {
   }
 
   private void initializeImages() {
-    desktop = ImageUtils.createObservable(desktopView,"background/desktop.jpg");
+    desktop = ObservableImage.create(desktopView,"background/desktop.jpg", BindingMode.BIDIRECTIONAL);
     desktop.control().width(Main.screenWidth);
+    desktop.control().opacity(1);
   }
 }
---
+++ src/main/java/samwilkins333/ScrabbleMini/FXML/Utilities/Image/ObservableImage.java
@@ -0,0 +1,38 @@
+package main.java.samwilkins333.ScrabbleMini.FXML.Utilities.Image;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import main.java.samwilkins333.ScrabbleMini.FXML.Scenes.Bindings.BindingMode;
+import main.java.samwilkins333.ScrabbleMini.FXML.Scenes.Bindings.Composite.ImageBindings;
+import main.resources.ResourceLoader;
+import main.resources.ResourceType;
+
+public class ObservableImage {
+  private ImageView view;
+  private ImageBindings bindings;
+
+  private ObservableImage(ImageView view, ImageBindings bindings) {
+    this.view = view;
+    this.bindings = bindings;
+  }
+
+  public ImageBindings control() {
+    return bindings;
+  }
+
+  public ImageView imageView() {
+    return view;
+  }
+
+  public static ObservableImage create(ImageView target, String location, BindingMode mode) {
+    String resource = ResourceLoader.instance.load(ResourceType.IMAGE, location);
+    target.setImage(new Image(resource));
+    target.setPreserveRatio(true);
+
+    ImageBindings bindings = new ImageBindings();
+    bindings.bind(target, mode);
+    bindings.cached(true);
+
+    return new ObservableImage(target, bindings);
+  }
+}
