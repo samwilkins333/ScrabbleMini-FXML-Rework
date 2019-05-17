7409d646462780fd2a33b68d20a37b30c4f73d8c
Sam Wilkins <samwilkins333@gmail.com>
2019-05-17 12:24:46 -0400

    tilebag config

--- src/main/java/samwilkins333/ScrabbleMini/Logic/Tiles/Initializer/TileReader.java
+++ src/main/java/samwilkins333/ScrabbleMini/Logic/Tiles/Initializer/TileReader.java
@@ -1,8 +1,19 @@
 package main.java.samwilkins333.ScrabbleMini.Logic.Tiles.Initializer;
 
+import main.resources.ResourceCreator;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class TileReader implements TileBagInitializer {
   @Override
-  public void initialize() {
-
+  public TileBagAttributes initialize() {
+    try {
+      BufferedReader reader = ResourceCreator.read("tilebag_config.txt");
+    } catch (IOException e) {
+      e.printStackTrace();
+      System.exit(1);
+    }
+    return new TileBagAttributes();
   }
 }
