443774e91011261b9da810470c1aaafde5516278
Sam Wilkins <samwilkins333@gmail.com>
2019-05-23 18:23:30 -0700

    completed resolving project-wide checkstyle errors!

--- src/module-info.java
+++ src/module-info.java
@@ -1,12 +1,12 @@
-//module ScrabbleMini {
-//  requires javafx.graphics;
-//  requires javafx.base;
-//  requires javafx.controls;
-//  requires javafx.fxml;
-//  requires kotlin.stdlib;
-//
-//  exports main.java.samwilkins333.ScrabbleMini.FXML.Controllers;
-//  exports main.java.samwilkins333.ScrabbleMini.FXML.Scenes.Bindings;
-//
-//  opens main.java.samwilkins333.ScrabbleMini;
-//}
+module ScrabbleMini {
+  requires javafx.graphics;
+  requires javafx.base;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires kotlin.stdlib;
+
+  exports main.java.samwilkins333.ScrabbleMini.FXML.Controllers;
+  exports main.java.samwilkins333.ScrabbleMini.FXML.Scenes.Bindings;
+
+  opens main.java.samwilkins333.ScrabbleMini;
+}
