18cafdc57f66d3e66e6fc59a04646cb11956b577
Sam Wilkins <samwilkins333@gmail.com>
2020-05-25 00:38:38 -0700

    cleanup

--- src/main/java/samwilkins333/ScrabbleMini/Logic/GameAgents/Referee/Initializer/GaddagInitializer.java
+++ src/main/java/samwilkins333/ScrabbleMini/Logic/GameAgents/Referee/Initializer/GaddagInitializer.java
@@ -7,7 +7,11 @@ import main.resources.ResourceCreator;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * An implementer of <code>DictionaryInitializer</code>
