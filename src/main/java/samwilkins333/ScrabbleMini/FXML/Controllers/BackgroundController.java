1de254d609efc5241f1c1e06a2ee296c32b515ee
Sam Wilkins <samwilkins333@gmail.com>
2020-05-31 18:10:35 -0700

    human v human

--- src/main/java/samwilkins333/ScrabbleMini/FXML/Controllers/BackgroundController.java
+++ src/main/java/samwilkins333/ScrabbleMini/FXML/Controllers/BackgroundController.java
@@ -92,9 +92,9 @@ public class BackgroundController implements Initializable {
    */
   public void begin() {
     PlayerList<GADDAG> players = new PlayerList<>(2);
-    players.add(new SimulatedPlayer(new MoveGenerator(), new MoveSelector()));
     players.add(new HumanPlayer());
-
+    players.add(new HumanPlayer());
+//    players.add(new SimulatedPlayer(new MoveGenerator(), new MoveSelector()));
     referee = new StandardReferee(players, board, tileBag);
   }
 }
